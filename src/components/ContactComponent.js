920220a7b1b11011d89db133c5a7abe5e7879977
Akshay A S <43413961+akshay512@users.noreply.github.com>
2020-07-31 11:44:19 +0530

    uncontrolled form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
-            Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+            Button, Form, FormGroup, FormFeedback, Label, Input, Col } from 'reactstrap';
 
 class Contact extends Component {
     
@@ -15,11 +15,18 @@ class Contact extends Component {
              email:'',
              agree:false,
              contactType:'Tel.',
-             message:''
+             message:'',
+             touched: {
+                 firstname: false,
+                 lastname:false,
+                 telnum: false,
+                 email:false,
+             }
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
 
     handleInputChange(event) {
@@ -32,6 +39,11 @@ class Contact extends Component {
         });
 
     }
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: {...this.state.touched, [field]: true}
+        });
+    }
 
 
     handleSubmit(event) {
@@ -39,9 +51,40 @@ class Contact extends Component {
         alert('Current State is: ' + JSON.stringify(this.state));
         event.preventDefault();
     }
+
+    validate(firstname,lastname,telnum,email) {
+        const errors ={
+            firstname:'',
+            lastname:'',
+            telnum:'',
+            email:'',
+        }
+
+        if(this.state.touched.firstname && firstname.length < 3)
+        errors.firstname = 'First Name should be >= 3 characters'
+        else if(this.state.touched.firstname && firstname.length > 10)
+        errors.firstname = 'First Name should be <= 10 characters'
+
+        if(this.state.touched.lastname && lastname.length < 3)
+        errors.lastname = 'Last Name should be >= 3 characters'
+        else if(this.state.touched.lastname && lastname.length > 10)
+        errors.lastname = 'Last Name should be <= 10 characters'
+
+        const reg = /^\d+$/;
+
+        if(this.state.touched.telnum && !reg.test(telnum))
+        errors.telnum='Tel. Number should contain only numbers';
+
+        if(this.state.touched.email && email.split('').filter(x => x ==='@').length !== 1)
+        errors.email = 'Email should contain a @';
+
+        return errors;
+    }
     
 
     render() {
+        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum,this.state.email);
+
         return (
             <div className="container">
             <div className="row">
@@ -94,7 +137,11 @@ class Contact extends Component {
                             <Col md={10}>
                                 <Input type="text" id="firstname" name="firstname" placeholder="First Name"
                                 value={this.state.firstname}
+                                valid={errors.firstname === ''}
+                                invalid={errors.firstname !==''}
+                                onBlur={this.handleBlur('firstname')}
                                 onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.firstname}</FormFeedback>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
@@ -102,7 +149,11 @@ class Contact extends Component {
                             <Col md={10}>
                                 <Input type="text" id="lastname" name="lastname" placeholder="Last Name"
                                 value={this.state.lastname}
+                                valid={errors.lastname === ''}
+                                invalid={errors.lastname !==''}
+                                onBlur={this.handleBlur('lastname')}
                                 onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.lastname}</FormFeedback>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
@@ -110,7 +161,11 @@ class Contact extends Component {
                             <Col md={10}>
                                 <Input type="tel" id="telnum" name="telnum" placeholder="Tel. number"
                                 value={this.state.telnum}
+                                valid={errors.telnum === ''}
+                                invalid={errors.telnum !==''}
+                                onBlur={this.handleBlur('telnum')}
                                 onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.telnum}</FormFeedback>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
@@ -118,7 +173,11 @@ class Contact extends Component {
                             <Col md={10}>
                                 <Input type="email" id="email" name="email" placeholder="Email"
                                 value={this.state.email}
+                                valid={errors.email === ''}
+                                invalid={errors.email !==''}
+                                onBlur={this.handleBlur('email')}
                                 onChange={this.handleInputChange} />
+                                <FormFeedback>{errors.email}</FormFeedback>
                             </Col>
                         </FormGroup>
 
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 
 class Header extends Component {
     constructor(props) {
@@ -8,8 +10,13 @@ class Header extends Component {
     
         this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-          isNavOpen: false
+          isNavOpen: false,
+          isModalOpen: false
         };
+
+        this.toggleModal = this.toggleModal.bind(this);
+
+        this.handleLogin = this.handleLogin.bind(this);
       }
 
       toggleNav() {
@@ -17,6 +24,21 @@ class Header extends Component {
           isNavOpen: !this.state.isNavOpen
         });
       }
+
+      toggleModal() {
+          this.setState({
+              isModalOpen: !this.state.isModalOpen
+          });
+      }
+
+      handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
+      
 
     render() {
         return(
@@ -41,6 +63,11 @@ class Header extends Component {
                             </NavItem>
                             </Nav>
                         </Collapse>
+                        <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
                     </div>
                 </Navbar>
                 <Jumbotron>
@@ -53,6 +80,31 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
