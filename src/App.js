f748912475ae9ff2b785035af6b852ee4d384941
Akshay A S <43413961+akshay512@users.noreply.github.com>
2020-08-01 10:01:35 +0530

    redux template

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import './App.css';
 import Main from './components/MainComponent';
 import { Route, Switch, NavLink, Link } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
-
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-  
   render() {
     return (
+      <Provider store={store} >
       <BrowserRouter>
       <div className="App">
       <Main />
     </div>
     </BrowserRouter>
+    </Provider>
     );
   }
 }
